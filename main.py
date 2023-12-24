f96f1b1be81372ddaab7fcadc1c2ea8bb778db91
thegreenpowerranger <110287825+thegreenpowerranger@users.noreply.github.com>
2023-12-24 09:49:10 +0100

    test

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+st.snow()
+def main():
+    st.title('Im Glanz des Werdens: Weihnachtliche Botschaften für dich')
+
+    st.markdown(
+        """
+        In der sternenklaren Weihnachtsnacht,
+        strahlt deine Vorfreude vollbracht.
+        Ein Wesen wächst, wie Buddha im Geist,
+        der Bauch, ein Rätsel, wie's wohl heißt?
+
+        Wie der Dalai Lama, so weise und mild,
+        vergräbst du dich in Büchern, als wär's dein Schild.
+        Mit Bauchumfang, der stetig wächst,
+        sind Weisheit und Windeln das, was dich jetzt begeist!
+
+        Die Lehren des Buddha, so tief und klar,
+        sind auch für Mamas ein heiliger Altar.
+        In dieser Zeit des werdenden Seins,
+        schenken Lachen und Liebe den Glanz, so fein.
+
+        Mit Buddha-Bauch und weiser Sicht,
+        sei unsere Weihnacht ein heiteres Licht.
+        Für dich, die werdende Mutter, mein Herz, mein Zuhause,
+        lässt uns dieser Segen der Weisheit nicht pausieren, sondern lächeln - Pause!
+        
+        Tenzin
+
+        """
+    )
+
+if __name__ == '__main__':
+    main()
