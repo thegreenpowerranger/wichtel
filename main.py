4a7a4ac68014220d21f02f777d92f788d1dd0f74
thegreenpowerranger <110287825+thegreenpowerranger@users.noreply.github.com>
2023-12-24 09:59:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,21 @@ def main():
         strahlt deine Vorfreude vollbracht.
         Ein Wesen wächst, wie Buddha im Geist,
         der Bauch, ein Rätsel, wie's wohl heißt?
-
+        
         Wie der Dalai Lama, so weise und mild,
         vergräbst du dich in Büchern, als wär's dein Schild.
         Mit Bauchumfang, der stetig wächst,
         sind Weisheit und Windeln das, was dich jetzt begeist!
-
+        
         Die Lehren des Buddha, so tief und klar,
         sind auch für Mamas ein heiliger Altar.
         In dieser Zeit des werdenden Seins,
         schenken Lachen und Liebe den Glanz, so fein.
-
+        
         Mit Buddha-Bauch und weiser Sicht,
         sei unsere Weihnacht ein heiteres Licht.
         Für dich, die werdende Mutter, mein Herz, mein Zuhause,
         lässt uns dieser Segen der Weisheit nicht pausieren, sondern lächeln - Pause!
-        
-        Tenzin
 
         """
     )
